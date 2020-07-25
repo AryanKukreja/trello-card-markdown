1d06322a1d7ee0f6c431c6c5cf40f5d75f36cf75
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 15:05:30 -0400

    Add base code for project

---
+++ src/js/connector.js
@@ -0,0 +1,29 @@
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+var onBtnClick = function (t, opts) {
+    console.log(t)
+    console.log(opts)
+    console.log('Someone clicked the button');
+};
+
+window.TrelloPowerUp.initialize({
+    'card-buttons': function (t, opts) {
+        console.log(t);
+        console.log(opts);
+        return [{
+            // usually you will provide a callback function to be run on button click
+            // we recommend that you use a popup on click generally
+            icon: GRAY_ICON, // don't use a colored icon here
+            text: 'Open Popup',
+            callback: onBtnClick,
+            condition: 'edit'
+        }, {
+            // but of course, you could also just kick off to a url if that's your thing
+            icon: GRAY_ICON,
+            text: 'Just a URL',
+            condition: 'always',
+            url: 'https://developer.atlassian.com/cloud/trello',
+            target: 'Trello Developer Site' // optional target for above url
+        }];
+    }
+});
