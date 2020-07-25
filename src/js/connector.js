28e2a2c8a8d64f6298d6467c22e50481006cb2c3
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 18:02:09 -0400

    Get all the card data - another trial

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,19 +2,20 @@ function convertCardToMarkdown(data) {
 
 }
 
-let onBtnClick = function (t, opts) {
+function onBtnClick(t) {
     console.log("yolo")
-};
+    console.log(JSON.stringify(t.card('all'), null, 2))
+}
 
 window.TrelloPowerUp.initialize({
-    'card-buttons': function (t, options) {
-        return t.get('card', 'shared')
-            .then(function(data) {
-                console.log(data);
+    'card-buttons': function (t, opts) {
+        return t.card('all')
+            .then(function (card) {
+                console.log(JSON.stringify(card, null, 2));
                 return [{
                     icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
                     text: 'Export to Markdown',
-                    callback: onBtnClick,
+                    callback: onBtnClick(t),
                     condition: 'always'
                 }];
             });
