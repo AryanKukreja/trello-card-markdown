adc1dbfdeaa4eb40278ef263a64eebee620e3b1c
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 16:31:32 -0400

    Get the current card

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,12 +1,11 @@
 let onBtnClick = function (t, opts) {
-    console.log(t.getContext())
+    const card = t.getContext().card;
+    console.log(card);
     console.log('Someone clicked the button');
 };
 
 window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
-        console.log(t);
-        console.log(opts);
         return [{
             icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
             text: 'Export to Markdown',
