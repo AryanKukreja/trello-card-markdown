c4fdd929597b9f2000a1a82beef0eca85ce173a8
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 17:45:05 -0400

    Get all the card data

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,22 +3,20 @@ function convertCardToMarkdown(data) {
 }
 
 let onBtnClick = function (t, opts) {
-    const card = t.getContext().card;
-    console.log(t.card(card))
-    return t.get(card)
-        .then(function (data) {
-            console.log(data);
-            console.log(JSON.stringify(data, null, 2));
-        });
+    console.log("yolo")
 };
 
 window.TrelloPowerUp.initialize({
-    'card-buttons': function (t, opts) {
-        return [{
-            icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
-            text: 'Export to Markdown',
-            callback: onBtnClick,
-            condition: 'always'
-        }];
+    'card-buttons': function (t, options) {
+        return t.get('card', 'shared')
+            .then(function(data) {
+                console.log(data);
+                return [{
+                    icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
+                    text: 'Export to Markdown',
+                    callback: onBtnClick,
+                    condition: 'always'
+                }];
+            });
     }
 });
