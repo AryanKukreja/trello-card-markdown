366120c13f3d9dbb3e3f6f9055d594be60be3010
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 21:19:17 -0400

    Attempt to fetc checklists

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,28 +5,32 @@ function convertCardToMarkdown(data) {
 
 }
 
-function onBtnClick(cardId) {
-    const url = 'https://api.trello.com/1/cards/' + cardId + '?key=' + process.env['TRELLO_KEY'] + '&token=' + process.env['TRELLO_TOKEN'];
-    console.log(url);
-
+function fectchCheckLists(url) {
     fetch(url, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
         }
     }).then(response => {
-            console.log(
-                `Response: ${response.status} ${response.statusText}`
-            );
-            return response.text();
-        })
-        .then(text => console.log(text))
-        .catch(err => console.error(err));
+        console.log(
+            `Response: ${response.status} ${response.statusText}`
+        );
+        return response.text();
+    }).catch(err => console.error(err));
+}
+
+function onBtnClick(cardId) {
+    const baseUrl = 'https://api.trello.com/1/cards/' + cardId;
+    const checkListUrl = 'https://api.trello.com/1/cards/' + cardId + '/checklists';
+    const boardUrl = 'https://api.trello.com/1/cards/' + cardId;
+    const authDetails = '?key=' + process.env['TRELLO_KEY'] + '&token=' + process.env['TRELLO_TOKEN']
+
+    let checkLists = fectchCheckLists(checkListUrl + authDetails)
+    console.log(checkLists)
 }
 
 window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
-        console.log(opts);
         return t.card('all')
             .then(function (card) {
                 console.log(card);
