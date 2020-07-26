25ef3b522b03679c408a7d0d86f587d84cef4269
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 20:38:10 -0400

    Get all the card data - another trial

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,3 +1,5 @@
+require('dotenv').config({path: '../../.env'});
+
 function convertCardToMarkdown(data) {
 
 }
@@ -12,7 +14,7 @@ window.TrelloPowerUp.initialize({
         console.log(opts);
         return t.card('all')
             .then(function (card) {
-                console.log(card.badges);
+                console.log(card);
                 return [{
                     icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
                     text: 'Export to Markdown',
