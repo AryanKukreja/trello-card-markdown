617481466668351f96c1509c581f3f113e3f0fcd
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 22:10:55 -0400

    Format checkLists

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,5 +1,6 @@
 require('dotenv').config({path: '../../.env'});
 const fetch = require('node-fetch');
+let markdownOutput = '';
 
 function convertCardToMarkdown(checkLists) {
     let markdownCheckLists = '# CheckLists\n';
@@ -20,7 +21,6 @@ function convertCardToMarkdown(checkLists) {
 }
 
 function fetchCheckLists(url) {
-    let results = null;
     fetch(url, {
         method: 'GET',
         headers: {
@@ -30,15 +30,10 @@ function fetchCheckLists(url) {
         console.log(
             `Response: ${response.status} ${response.statusText}`
         );
-        return response.text();
-    })
-    .then(text => {
-        console.log(text);
-        results = text;
+        markdownOutput += JSON.parse(response.text());
+        console.log(markdownOutput)
     })
     .catch(err => console.error(err));
-
-    return results;
 }
 
 function onBtnClick(cardId) {
@@ -47,17 +42,13 @@ function onBtnClick(cardId) {
     const boardUrl = 'https://api.trello.com/1/cards/' + cardId;
     const authDetails = '?key=' + process.env['TRELLO_KEY'] + '&token=' + process.env['TRELLO_TOKEN']
 
-    const checkLists = JSON.parse(fetchCheckLists(checkListUrl + authDetails));
-
-    let markdownCheckLists = convertCardToMarkdown(checkLists);
-    console.log(markdownCheckLists);
+    fetchCheckLists(checkListUrl + authDetails);
 }
 
 window.TrelloPowerUp.initialize({
     'card-buttons': function (t, opts) {
         return t.card('all')
             .then(function (card) {
-                console.log(card);
                 return [{
                     icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
                     text: 'Export to Markdown',
