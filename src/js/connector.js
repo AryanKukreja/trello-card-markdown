d59d5d0f49783fb657de5c21cf8ab13b5a9e1605
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-26 10:44:02 -0400

    Extract other Info for Markdown Document

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,27 +1,86 @@
 require('dotenv').config({path: '../../.env'});
 const fetch = require('node-fetch');
+
+const labelTemplate = '<p style="border-style: solid; border-color: ENTER_COLOR_HERE; border-radius: 10px; padding: 4px; background: ENTER_COLOR_HERE; color: white"><b>ENTER_LABEL_NAME_HERE</b></p>'
+const memberTemplate = '<b style="background-color: #9b9c9e; display: inline-block; border-radius: 50%; width: 40px; height: 40px; line-height: 40px; text-align: center;">ENTER_MEMBER_HERE</b>'
+const colors = {
+    "green": "#61BD4F",
+    "yellow": "#F2D600",
+    "orange": "#FF9F1A",
+    "red": "#EB5A46",
+    "purple": "#C377E0",
+    "blue": "#0079BF",
+    "sky": "#00C2E0",
+    "lime": "#51E898",
+    "pink": "#FF78CB",
+    "black": "#344563",
+}
+
 let markdownOutput = '';
 let cardId = '';
 
-function convertCardToMarkdown(checkLists) {
-    let markdownCheckLists = '# CheckLists\n';
-    checkLists.forEach((checkList) => {
-        let markdownCheckList = '## CheckList: \'' + checkList.name + '\'\n';
-        checkList.checkItems.forEach((item) => {
-            if (item.state === 'incomplete') {
-                markdownCheckList += ' - [ ] ' + item.name + '\n';
-            }
-            else {
-                markdownCheckList += ' - [x] ' + item.name + '\n';
-            }
+function addCheckListToOutput(checkLists) {
+    let markdownCheckLists = '';
+    if (checkLists.length > 0) {
+        markdownCheckLists = '# CheckLists\n';
+        checkLists.forEach((checkList) => {
+            let markdownCheckList = '## CheckList: \'' + checkList.name + '\'\n';
+            checkList.checkItems.forEach((item) => {
+                if (item.state === 'incomplete') {
+                    markdownCheckList += ' - [ ] ' + item.name + '\n';
+                }
+                else {
+                    markdownCheckList += ' - [x] ' + item.name + '\n';
+                }
+            });
+            markdownCheckLists += markdownCheckList + '\n';
         });
-        markdownCheckLists += markdownCheckList + '\n';
-    });
+    }
 
     return markdownCheckLists;
 }
 
-function fectchCheckLists(url) {
+function addCardDetailsToOutput(cardInfo) {
+    let markdownCard = '# ' + cardInfo.name + '\n';
+
+    // Add due date
+    markdownCard += 'Due on: ' +  cardInfo.due.split('T')[0] + '\n\n';
+
+    // Add card description
+    markdownCard += cardInfo.desc + '\n\n';
+
+    // Add labels
+    if (cardInfo.labels.length > 0) {
+        markdownCard += 'Labels: \n';
+        cardInfo.labels.forEach((label) => {
+            markdownCard += labelTemplate.replace(/ENTER_COLOR_HERE/g, colors[label.color]).replace(/ENTER_LABEL_NAME_HERE/g, label.name) + ' ';
+        });
+    }
+
+    markdownOutput += markdownCard + '\n\n';
+
+    return markdownCard;
+}
+
+function addMembersToOutput(memberInfo) {
+    let markdownMember = '';
+    if (memberInfo.length > 0) {
+        markdownMember += '## Members of Card: '
+        memberInfo.forEach((member) => {
+            markdownMember += memberTemplate.replace(/ENTER_MEMBER_HERE/g, member.initials) + ' ' + member.name + '\n';
+        })
+    }
+}
+
+function addBoardToOutput(boardInfo) {
+    markdownOutput += boardInfo.name + (boardInfo.name.toLowerCase().includes('board') ? '' : ' Board');
+}
+
+function addListToOutput(listInfo) {
+    markdownOutput += listInfo.name + (listInfo.name.toLowerCase().includes('list') ? '' : ' List') + '\n\n';
+}
+
+function fetchData(url, dataType) {
     fetch(url, {
         method: 'GET',
         headers: {
@@ -35,7 +94,22 @@ function fectchCheckLists(url) {
     })
     .then((text) => {
         console.log(JSON.parse(text));
-        console.log(convertCardToMarkdown(JSON.parse(text)))
+
+        if (dataType === 'checklist') {
+            console.log(addCheckListToOutput(JSON.parse(text)));
+        }
+        else if (dataType === 'board') {
+            console.log(addBoardToOutput(JSON.parse(text)));
+        }
+        else if (dataType === 'list') {
+            console.log(addListToOutput(JSON.parse(text)));
+        }
+        else if (dataType === 'card') {
+            console.log(addCardDetailsToOutput(JSON.parse(text)));
+        }
+        else if (dataType === 'members') {
+            console.log(addMembersToOutput(JSON.parse(text)));
+        }
     })
     .catch(err => console.error(err));
 }
@@ -44,11 +118,18 @@ onBtnClick = function(t, opts) {
     console.log(t)
     console.log(opts)
     const baseUrl = 'https://api.trello.com/1/cards/' + cardId;
-    const checkListUrl = 'https://api.trello.com/1/cards/' + cardId + '/checklists';
-    const boardUrl = 'https://api.trello.com/1/cards/' + cardId;
-    const authDetails = '?key=' + process.env['TRELLO_KEY'] + '&token=' + process.env['TRELLO_TOKEN']
+    const authDetails = '?key=' + process.env['TRELLO_KEY'] + '&token=' + process.env['TRELLO_TOKEN'];
 
-    fectchCheckLists(checkListUrl + authDetails)
+    const checkListUrl = baseUrl + '/checklists';
+    const boardUrl = baseUrl + '/board';
+    const listUrl = baseUrl + '/list';
+    const memberUrl = baseUrl + '/members';
+
+    fetchData(baseUrl + authDetails, 'card')
+    fetchData(boardUrl + authDetails, 'board')
+    fetchData(listUrl + authDetails, 'list')
+    fetchData(memberUrl + authDetails, 'members')
+    fetchData(checkListUrl + authDetails, 'checklist')
 }
 
 window.TrelloPowerUp.initialize({
