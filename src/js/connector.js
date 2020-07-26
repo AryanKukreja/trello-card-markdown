afc4ae701e7a1998d18aa28c3f7d1977d5c5e69c
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 22:53:27 -0400

    Make button work

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: '../../.env'});
 const fetch = require('node-fetch');
 let markdownOutput = '';
+let cardId = '';
 
 function convertCardToMarkdown(checkLists) {
     let markdownCheckLists = '# CheckLists\n';
@@ -55,6 +56,7 @@ window.TrelloPowerUp.initialize({
         return t.card('all')
             .then(function (card) {
                 console.log(card);
+                cardId = card.id;
                 return [{
                     icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
                     text: 'Export to Markdown',
