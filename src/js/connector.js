09ca2981e31333b9ca06322d2b4055d9582c570a
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-26 18:18:10 -0400

    Try to get rid of "page not found" error

--- src/js/connector.js
+++ src/js/connector.js
@@ -177,7 +177,7 @@ window.TrelloPowerUp.initialize({
                     callback: function(t) {
                         return t.popup({
                             title: "Select Fields",
-                            url: "../html/index.html"
+                            url: "index.html"
                         })
                     },
                     condition: 'always'
