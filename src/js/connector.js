ccffb2b7e72f71ee611c2507ab7fc9cf6564f109
100651838 <aryan.kukreja@ontariotechu.net>
2020-07-25 20:50:38 -0400

    Attempt to get card data

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,12 +1,27 @@
 require('dotenv').config({path: '../../.env'});
+const fetch = require('node-fetch');
 
 function convertCardToMarkdown(data) {
 
 }
 
-function onBtnClick(t) {
-    console.log("yolo")
-    console.log(JSON.stringify(t.card('all'), null, 2))
+function onBtnClick(cardId) {
+    const url = 'https://api.trello.com/1/cards/' + cardId + '?key=' + process.env['TRELLO_KEY'] + '&token=' + process.env['TRELLO_TOKEN'];
+    console.log(url);
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+            console.log(
+                `Response: ${response.status} ${response.statusText}`
+            );
+            return response.text();
+        })
+        .then(text => console.log(text))
+        .catch(err => console.error(err));
 }
 
 window.TrelloPowerUp.initialize({
@@ -18,7 +33,7 @@ window.TrelloPowerUp.initialize({
                 return [{
                     icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg', // don't use a colored icon here
                     text: 'Export to Markdown',
-                    callback: onBtnClick(t),
+                    callback: onBtnClick(card.id),
                     condition: 'always'
                 }];
             });
