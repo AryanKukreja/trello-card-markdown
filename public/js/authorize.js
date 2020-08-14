4bba02b8be211b9338ecdb55266c50449ba9d0c9
100651838 <aryan.kukreja@ontariotechu.net>
2020-08-13 23:20:47 -0400

    Bug fix

---
+++ public/js/authorize.js
@@ -0,0 +1,22 @@
+require('dotenv').config({path: '../.env'});
+let Promise = TrelloPowerUp.Promise;
+let t = TrelloPowerUp.iframe();
+
+let apiKey = process.env['TRELLO_KEY']; // Passed in as an argument to our iframe
+
+let trelloAuthUrl = `https://trello.com/1/authorize?expiration=neverr&name=Card%20To%20Markdown&scope=read&key=${apiKey}&callback_method=fragment&return_url=${window.location.origin}%2Fauth-success.html`;
+
+let tokenLooksValid = function(token) {
+    // If this returns false, the Promise won't resolve.
+    return /^[0-9a-f]{64}$/.test(token);
+}
+
+document.getElementById('auth-btn').addEventListener('click', function(){
+    t.authorize(trelloAuthUrl, { height: 680, width: 580, validToken: tokenLooksValid })
+        .then(function(token){
+            return t.set('member', 'private', 'token', token)
+        })
+        .then(function(){
+            return t.closePopup();
+        });
+});
